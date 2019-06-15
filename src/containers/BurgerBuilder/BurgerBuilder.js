8e496044358f1f004271e9c97218768dfccd2a78
F1renDev <47530368+F1renDev@users.noreply.github.com>
2019-06-15 21:14:32 +0300

    Making some dummy requests to firebase with axios
    Making some dummy requests to firebase with axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import React from "react";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -26,7 +27,7 @@ class BurgerBuilder extends React.Component {
 
     updatePurchaseState = (ingredients) => {
         const sum = Object.keys(ingredients)
-            .map(item => {
+            .map((item) => {
                 return ingredients[item];
             })
             .reduce((sum, el) => {
@@ -77,21 +78,37 @@ class BurgerBuilder extends React.Component {
     purchaseHandler = () => {
         this.setState({
             purchasing: true
-        })
+        });
     };
 
     purchaseCancelHandler = () => {
         this.setState({
             purchasing: false
-        })
+        });
     };
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const price = this.state.totalPrice.toFixed(2);
+        const order = {
+            ingredients: this.state.ingredients,
+            price: price,
+            customer: {
+                name: "Customer-1",
+                adress: {
+                    street: "CustomerStreet-1"
+                },
+                phone: "322223",
+                email: "CustomerEmail@1.com"
+            },
+            deliveryMethod: "fastest"
+        };
+        axios.post("/orders.json", order)
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
     };
 
     render() {
-
         const disabledInfo = {
             ...this.state.ingredients
         };
@@ -101,14 +118,17 @@ class BurgerBuilder extends React.Component {
 
         return (
             <React.Fragment>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                <Modal
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary
                         price={this.state.totalPrice.toFixed(2)}
                         purchaseCancelled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}
-                        ingredients={this.state.ingredients}/>
+                        ingredients={this.state.ingredients}
+                    />
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
+                <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     price={this.state.totalPrice}
                     ingredientRemoved={this.removeIngredientHandler}
